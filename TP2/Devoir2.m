52777064340958f99b44b7da132bca3443fdea95
Vincent Audet <audetvincent@gmail.com>
2017-10-23 17:49:04 -0400

    add 3D

--- TP2/Devoir2.m
+++ TP2/Devoir2.m
@@ -15,10 +15,15 @@ trace = Trace();
 [coup, trace] = Simulation(1, Balle, Obstacles, trace);
 
 tf = trace.t(end);
-rbf = trace.position(:, end);
-vbf = trace.vitesse(:, end);
+rbf = trace.position(:, end)';
+vbf = trace.vitesse(:, end)';
 
+disp("coup");
+disp(coup);
+disp("Temps Finale");
 disp(tf);
+disp("Position Finale");
 disp(rbf);
+disp("Vitesse Finale");
 disp(vbf);
 end
--- TP2/main.m
+++ TP2/main.m
@@ -2,16 +2,7 @@ clear;  %clear la mémoire
 clc;    %clear le texte de la console
 clf;
 
-hold on
-axis([0 3 0 1.5]);
-%table
-x = [0 2.74];
-y = [0.76 0.76];
-fill1 = fill(x, y, 'r');
-
-a = [1.37 1.37];
-b = [0.76 0.9125];
-fill2 = fill(a, b, 'r');
+DrawTable();
 
 for i = 1:3
     initialisation();
@@ -19,11 +10,12 @@ for i = 1:3
     for j = 1:4
         trace = Trace();
         [coup, trace] = Simulation(i, Balles(j), Obstacles, trace);
-        plot(trace.position(1,:), trace.position(3,:));
+        plot3(trace.position(1,:), trace.position(2,:), trace.position(3,:));
         resultat = [resultat; coup];
     end
     disp("Resultat");
     disp(resultat);
+    
 end
 
 
