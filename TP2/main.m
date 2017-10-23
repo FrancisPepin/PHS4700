4320435b708cea02e81d6bda5f00c155b95a0029
Vincent Audet <audetvincent@gmail.com>
2017-10-23 17:57:40 -0400

    added result

--- TP2/main.m
+++ TP2/main.m
@@ -3,18 +3,16 @@ clc;    %clear le texte de la console
 clf;
 
 DrawTable();
-
+resultat = [];
 for i = 1:3
     initialisation();
-    resultat = [];
     for j = 1:4
         trace = Trace();
         [coup, trace] = Simulation(i, Balles(j), Obstacles, trace);
         plot3(trace.position(1,:), trace.position(2,:), trace.position(3,:));
-        resultat = [resultat; coup];
+
+        resultat = [resultat; coup trace.t(end) trace.position(end) trace.vitesse(end)];
     end
-    disp("Resultat");
-    disp(resultat);
     
 end
 
